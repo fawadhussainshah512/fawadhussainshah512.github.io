43202639495953a7dd21dfbef2be865ba582836c
Fawad Hussain Shah <107517943+fawadhussainshah512@users.noreply.github.com>
2024-08-05 20:42:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
 
 const typed=new Typed('.multiple-text',
 {
-    strings:['Mobile App Developer'],
+    strings:['Flutter Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
